0a3a41d9e8b3efc97457f79dcdd6b5e1b27eb59c
RyanGBC <ryangabrj@gmail.com>
2025-09-10 18:41:11 -0300

    resolved audio

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const tracks = [
   { name: "Audio Endless tale", file: "audios/2_The Endless Tale Natalie Complete Audio.mp3" },
   { name: "Audio Beanstalk", file: "audios/3_Jack and the Beanstalk Natalie Complete Audio.mp3" },
   {name: "Audio The boy flew too high", file: "audios/4_The Boy who Flew too High Natalie Complete Audio.mp3"},
-  {name: "Audio The Bell of Atri", flie: "audios/5_The Bell of Atri Natalie Complete Audio.mp3"}
+  {name: "Audio The Bell of Atri", file: "5_The Bell of Atri Kathy Complete Audio.mp3"}
 ];
 
 const jackHannafordTranscript = [
