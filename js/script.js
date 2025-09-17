33b77799bf2016f1943170b59728e4508e29584c
RyanGBC <ryangabrj@gmail.com>
2025-09-17 12:04:23 -0300

    increase delay for better visibility of transcript display

--- js/script.js
+++ js/script.js
@@ -278,7 +278,7 @@ audioPlayer.addEventListener("timeupdate", () => {
   const current = audioPlayer.currentTime;
   [...textlist.children].forEach(li => {
     const start = parseFloat(li.dataset.start );
-    const end = parseFloat(li.dataset.end +0.15); // pequeno delay para melhor visualização
+    const end = parseFloat(li.dataset.end +0.30); // pequeno delay para melhor visualização
     li.classList.toggle("active", current >= start && current <= end);
   });
 });
