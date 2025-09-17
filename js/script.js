e39076f1ca4a060641e202e3d440a7f2e8731e9c
RyanGBC <ryangabrj@gmail.com>
2025-09-17 12:07:46 -0300

    adjust for 30

--- js/script.js
+++ js/script.js
@@ -277,7 +277,7 @@ function showTranscript(transcript) {
 audioPlayer.addEventListener("timeupdate", () => {
   const current = audioPlayer.currentTime;
   [...textlist.children].forEach(li => {
-    const start = parseFloat(li.dataset.start +0.75);
+    const start = parseFloat(li.dataset.start +0.30);
     const end = parseFloat(li.dataset.end +0.55); // pequeno delay para melhor visualização
     li.classList.toggle("active", current >= start && current <= end);
   });
