21609f0d19a307e714cd65eb10ac1587e4950fb0
RyanGBC <ryangabrj@gmail.com>
2025-09-17 12:00:47 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -277,7 +277,7 @@ function showTranscript(transcript) {
 audioPlayer.addEventListener("timeupdate", () => {
   const current = audioPlayer.currentTime;
   [...textlist.children].forEach(li => {
-    const start = parseFloat(li.dataset.start -1);
+    const start = parseFloat(li.dataset.start +11);
     const end = parseFloat(li.dataset.end);
     li.classList.toggle("active", current >= start && current <= end);
   });
