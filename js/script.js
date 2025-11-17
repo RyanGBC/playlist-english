0562ae762d9c63645bda2ca063aeddafe82ca20c
RyanGBC <ryangabrj@gmail.com>
2025-11-17 16:17:12 -0300

    Refactor code structure for improved readability and maintainability

--- js/script.js
+++ js/script.js
@@ -11,7 +11,8 @@ const tracks = [
   { name: "Steve Jobs Commencement Address", file: "audios/10_Steve Jobs Commencement Address.mp3" },
   { name: "A Change in Routine 1", file: "audios/11_AUDIO A Change in Routine 01 Josh.mp3" },
   { name: "A Change in Routine 2", file: "audios/12_AUDIO A Change in Routine 02 Josh.mp3" },
-  { name: "A Change in Routine 3", file: "audios/13_AUDIO A Change in Routine 03 Josh.mp3" }
+  { name: "A Change in Routine 3", file: "audios/13_AUDIO A Change in Routine 03 Josh.mp3" },
+  { name: "A Change in Routine 4", file: "audios/14_AUDIO A Change in Routine 04 Josh.mp3" }
 ];
 
 /*const jackHannafordTranscript = [
