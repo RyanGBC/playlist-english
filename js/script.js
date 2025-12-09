696504ff03acc0e3201915f9f2083b0572a8240d
RyanGBC <ryangabrj@gmail.com>
2025-12-09 18:32:36 -0300

    Refactor code structure the name

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ const tracks = [
   { name: "A Change in Routine", file: "audios/11_AUDIO COMPLETO A Change in Routine Josh.mp3" },
   { name: "Hills like white elephants", file: "audios/12_AUDIO COMPLETO Hills Like White Elephants Jacob.mp3" },
   { name: "The Red Bandits of Montgomery 1", file: "audios/13_AUDIO The Red Bandits of Montgomery 01 Jacob.mp3" },
-  { name: "The Red Bandits of Montgomery 2", file: "audios/13_AUDIO The Red Bandits of Montgomery 02 Jacob.mp3" }
+  { name: "The Red Bandits of Montgomery 2", file: "audios/14_AUDIO The Red Bandits of Montgomery 02 Jacob.mp3" }
 ];
 
 /*const jackHannafordTranscript = [
